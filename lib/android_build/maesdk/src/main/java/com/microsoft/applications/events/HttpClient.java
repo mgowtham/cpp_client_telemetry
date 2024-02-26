d7898c35e608073affaa03e6093ddefa969a807c
mgowtham <gowtham.manivannan@gmail.com>
2024-02-26 17:52:19 +0530

    Update
    Test

--- lib/android_build/maesdk/src/main/java/com/microsoft/applications/events/HttpClient.java
+++ lib/android_build/maesdk/src/main/java/com/microsoft/applications/events/HttpClient.java
@@ -33,6 +33,7 @@ import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
